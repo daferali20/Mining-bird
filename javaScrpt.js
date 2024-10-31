30e3844ed26528f519f1e7efe0349d76fb4ff0e3
daferali20 <31751340+daferali20@users.noreply.github.com>
2024-10-31 22:56:08 +0300

    Update javaScrpt.js

--- javaScrpt.js
+++ javaScrpt.js
@@ -212,6 +212,24 @@
 		"stateMutability": "view",
 		"type": "function"
 	}
+		{
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            },
+            {
+                "internalType": "address payable",
+                "name": "recipient",
+                "type": "address"
+            }
+        ],
+        "name": "withdraw",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
 ];
         // الاتصال بالمحفظة
 async function connectWallet() {
