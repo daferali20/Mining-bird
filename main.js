86a7bd5cf8438b90b96efe0e5ac4383d19542356
daferali20 <31751340+daferali20@users.noreply.github.com>
2024-11-02 21:17:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,17 @@ let contract;
 // الاتصال بمحفظة MetaMask
 async function connectWallet() {
     if (window.ethereum) {
-        web3 = new Web3(window.ethereum);
         try {
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
-            const accounts = await web3.eth.getAccounts();
-            account = accounts[0];
-            console.log("Connected account:", account);
+            // يطلب الإذن للوصول إلى الحسابات
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            console.log("Connected account:", accounts[0]);
+        } catch (error) {
+            console.error("User denied account access", error);
+        }
+    } else {
+        alert("Please install MetaMask to use this feature.");
+    }
+}
 
             // إعداد العقد باستخدام `contractAddress` و `contractABI`
             if (typeof contractAddress !== 'undefined' && typeof contractABI !== 'undefined') {
